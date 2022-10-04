b3f7b702b87f1404386388a018b83c1cbd848b04
Valentin447 <valentin@myttk.ru>
2022-10-04 18:12:50 +0700

    Add solution Task43

--- Program.cs
+++ Program.cs
@@ -6,28 +6,51 @@
     Console.WriteLine("|  Введите букву чтобы завершить ввод.                   |");
     Console.WriteLine("---------------------------------------------------------");
     Positive();
-    
+
 }
 void Task43()
 {
-    
+    // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+    // значения b1, k1, b2 и k2 задаются пользователем.
+
+    Console.Write("Введите b1: ");
+    double b1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите k1: ");
+    double k1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите b2: ");
+    double b2 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите k2: ");
+    double k2 = Convert.ToDouble(Console.ReadLine());
+    if (k1 == k2)
+    {
+        Console.WriteLine("Прямые не пересекаются.");
+    }
+    else
+    {
+        double x = Math.Round((b2 - b1) / (k1 - k2), 3);
+        double y = Math.Round(k1 * x + b1, 3);
+        Console.WriteLine("Координаты точки пересечения: x = " + x + "  y = " + y);
+    }
 }
 
-void Positive(int count = 0){
+void Positive(int count = 0)
+{
     string str = Console.ReadLine();
-    if(int.TryParse(str, out int number))
+    if (int.TryParse(str, out int number))
     {
-        if(number > 0)
+        if (number > 0)
         {
             count++;
         }
         Positive(count);
-    } 
+    }
     else
     {
         Console.WriteLine("Положительных чисел: " + count);
         return;
     }
 }
-Task41();
-//Task43();
+
+
+//Task41();
+Task43();
